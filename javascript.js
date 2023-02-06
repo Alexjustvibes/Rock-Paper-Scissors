fa8f1244738fc9bbf353acf62ce8f69ff405c4ad
Alexjustvibes <amaldonado7678@gmail.com>
2023-02-06 11:53:50 -0800

    Created One Round Of Play

---
+++ javascript.js
@@ -0,0 +1,45 @@
+
+function getComputerChoice(min, max) {
+    const computerChoice = Math.floor(Math.random() * (max - min) ) + min;
+
+    if(computerChoice == 1){
+        return("rock");
+    }
+    else if(computerChoice == 2){
+        return("scissors");
+    }
+    else{
+        return("paper");
+    }
+}
+
+
+function getPlayerChoice(){
+    let playerChoice= prompt("Rock, Paper, Scissors!");
+    return playerChoice.toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection);
+    console.log(computerSelection);
+    if(playerSelection === computerSelection){
+        return("tie");
+  }
+  else if(playerSelection === "rock" && computerSelection === "scissors"){
+    return("win");
+  }
+  else if(playerSelection === "paper" && computerSelection === "rock"){
+    return("win");
+  }
+  else if(playerSelection === "scissors" && computerSelection === "paper"){
+    return("win");
+  }
+  else{
+    return("lose");
+  }
+}
+
+   
+  const playerSelection = getPlayerChoice();
+  const computerSelection = getComputerChoice(1,4);
+  console.log(playRound(playerSelection, computerSelection));
